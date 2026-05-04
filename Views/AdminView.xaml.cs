965419f8cf4b8c319ace48aec7144abafcf354e8
renata garcia <renatagarcia3905@gmail.com>
2026-05-04 03:03:34 -0600

    actualzacion de diseño

--- Views/AdminView.xaml.cs
+++ Views/AdminView.xaml.cs
@@ -1,6 +1,7 @@
+using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using PuntoVenta.Views;
+using Microsoft.UI.Xaml.Media.Animation;
 
 namespace PuntoVenta.Views
 {
@@ -37,10 +38,51 @@ namespace PuntoVenta.Views
             MainWindow.Instance.MainFrameControl.Navigate(typeof(DifferencesView));
         }
 
-        // 🚪 Logout
+        // FUNCION DE ANIMACION PARA LA VENTANA DE ADMINISTRADOR
         private void Logout_Click(object sender, RoutedEventArgs e)
         {
             MainWindow.Instance.MainFrameControl.Navigate(typeof(LoginView));
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            AnimarBoton(BtnCrearEmpleado, 0);
+            AnimarBoton(BtnCrearProducto, 150);
+            AnimarBoton(BtnReportes, 300);
+            AnimarBoton(BtnDiferencias, 450);
+            AnimarBoton(BtnCerrarSesion, 600);
+        }
+
+        private void AnimarBoton(Button boton, int delay)
+        {
+            Storyboard storyboard = new Storyboard();
+
+            DoubleAnimation animacionMovimiento = new DoubleAnimation
+            {
+                From = 60,
+                To = 0,
+                Duration = new Duration(TimeSpan.FromMilliseconds(500)),
+                BeginTime = TimeSpan.FromMilliseconds(delay)
+            };
+
+            DoubleAnimation animacionOpacidad = new DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = new Duration(TimeSpan.FromMilliseconds(500)),
+                BeginTime = TimeSpan.FromMilliseconds(delay)
+            };
+
+            Storyboard.SetTarget(animacionMovimiento, boton);
+            Storyboard.SetTargetProperty(animacionMovimiento, "(UIElement.RenderTransform).(TranslateTransform.Y)");
+
+            Storyboard.SetTarget(animacionOpacidad, boton);
+            Storyboard.SetTargetProperty(animacionOpacidad, "Opacity");
+
+            storyboard.Children.Add(animacionMovimiento);
+            storyboard.Children.Add(animacionOpacidad);
+
+            storyboard.Begin();
         }
     }
 }
