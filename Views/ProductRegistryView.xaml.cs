cf7bf2ef5a21b7483beb3db1dcac928971c01957
1Anuk1 <juan.lujanomln@uanl.edu.mx>
2026-05-05 16:59:33 -0600

    Boton de eliminar producto añadido

--- Views/ProductRegistryView.xaml.cs
+++ Views/ProductRegistryView.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.UI.Xaml.Controls;
 using PuntoVenta.Models;
 using PuntoVenta.Services;
 using System.Collections.Generic;
+using System;
 
 namespace PuntoVenta.Views
 {
@@ -29,6 +30,35 @@ namespace PuntoVenta.Views
                 var win = new EditProductWindow(product);
                 win.Activate();
             }
+        }
+
+        private async void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button || button.DataContext is not Product product)
+            {
+                return;
+            }
+
+            var confirmDialog = new ContentDialog
+            {
+                Title = "Eliminar producto",
+                Content = $"¿Deseas eliminar el producto {product.Nombre}?",
+                PrimaryButtonText = "Eliminar",
+                CloseButtonText = "Cancelar",
+                XamlRoot = this.XamlRoot
+            };
+
+            var result = await confirmDialog.ShowAsync();
+            if (result != ContentDialogResult.Primary)
+            {
+                return;
+            }
+
+            products.Remove(product);
+            await JsonService.SaveAsync("products.json", products);
+
+            ProductsList.ItemsSource = null;
+            ProductsList.ItemsSource = products;
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
