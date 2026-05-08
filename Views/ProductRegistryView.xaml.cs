3190c453b8424d4b8c3650eebc368823a6eeeedd
1Anuk1 <juan.lujanomln@uanl.edu.mx>
2026-05-08 05:19:35 -0600

    Funcion de boton de reabastecimiento creada

--- Views/ProductRegistryView.xaml.cs
+++ Views/ProductRegistryView.xaml.cs
@@ -4,6 +4,7 @@ using PuntoVenta.Models;
 using PuntoVenta.Services;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace PuntoVenta.Views
 {
@@ -30,6 +31,63 @@ namespace PuntoVenta.Views
                 var win = new EditProductWindow(product);
                 win.Activate();
             }
+        }
+
+        private async void Restock_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button || button.DataContext is not Product product)
+            {
+                return;
+            }
+
+            var amountBox = new TextBox
+            {
+                PlaceholderText = "Cantidad a añadir"
+            };
+
+            amountBox.BeforeTextChanging += (s, e2) =>
+            {
+                string text = e2.NewText;
+
+                if (text.Any(c => !char.IsDigit(c)))
+                {
+                    e2.Cancel = true;
+                }
+            };
+
+            var dialog = new ContentDialog
+            {
+                Title = "Reabastecimiento",
+                Content = amountBox,
+                PrimaryButtonText = "Agregar",
+                CloseButtonText = "Cancelar",
+                XamlRoot = this.XamlRoot
+            };
+
+            var result = await dialog.ShowAsync();
+            if (result != ContentDialogResult.Primary)
+            {
+                return;
+            }
+
+            if (!int.TryParse(amountBox.Text, out int amount) || amount <= 0)
+            {
+                await new ContentDialog
+                {
+                    Title = "Error",
+                    Content = "Ingresa una cantidad válida mayor a 0",
+                    CloseButtonText = "OK",
+                    XamlRoot = this.XamlRoot
+                }.ShowAsync();
+                return;
+            }
+
+            product.Existencias += amount;
+
+            await JsonService.SaveAsync("products.json", products);
+
+            ProductsList.ItemsSource = null;
+            ProductsList.ItemsSource = products;
         }
 
         private async void Delete_Click(object sender, RoutedEventArgs e)
