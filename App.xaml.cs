026ae885581cd3f4181e0618009b60feadafa68a
renata garcia <renatagarcia3905@gmail.com>
2026-05-10 03:03:57 -0600

    nada login

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,29 +1,10 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using PuntoVenta.Views;
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace PuntoVenta
 {
     public partial class App : Application
     {
-        private Window m_window;
+        private Window? m_window;
 
         public App()
         {
@@ -34,6 +15,7 @@ namespace PuntoVenta
         {
             m_window = new MainWindow();
             m_window.Activate();
+            m_window.Title = "ClickCompra";
         }
     }
 }
